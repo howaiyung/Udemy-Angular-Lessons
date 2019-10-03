cdce6f26385fa319b6d768fc061691bd5cb90b53
Ho Wai Yung <howaiyung1988@gmail.com>
2019-10-02 22:27:13 -0600

    Udemy - Angular 8 - Section 2 - Lesson 15 - Creating a New Component

--- Udemy/my-first-app/src/app/app.module.ts
+++ Udemy/my-first-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,8 +10,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// Refer to https://stackoverflow.com/questions/45129790/difference-between-httpmodule-and-httpclientmodule/45129865#45129865
+//
