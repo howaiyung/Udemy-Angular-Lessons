c79ad52a6d13c6fa7f36f1cebbbfb65fe9658b11
Ho Wai Yung <howaiyung1988@gmail.com>
2019-11-14 15:09:55 -0700

    Udemy - Angular 8 - Section 2 - Lesson 22 - Fully Understanding the Component Selector

--- Udemy/my-first-app/src/app/servers/servers.component.ts
+++ Udemy/my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', //Angular selects by element
+  //selector: '[app-servers]', //Angular selects by attribute
+  //selector: '.app-servers', //Angular selects by class
+
   /*templateUrl: './servers.component.html',*/
   template: `
   <app-server></app-server>
@@ -23,5 +26,17 @@ export class ServersComponent implements OnInit {
 In components, '' or single quotations are for single line templates
              , `` or back quotations are for multi line template components inline
              Rule of thumb is if you have more than 3 templates using in this component
+
+*/
+
+/*
+
+By putting square brackets on a element in selector:, to use attribute selector. CSS select elements by attribute.
+It changes app-server from an element to a attribute.
+
+Angular selects the element by attribute and not by itself since we change the selector for the element.
+
+Selecting by id is not supported by Angular.
+Psuedo selectors ;ike Hover is not supported by Angular
 
 */
