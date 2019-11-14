153220dc39258d7050718ff3b0e3674a8374f27f
Ho Wai Yung <howaiyung1988@gmail.com>
2019-11-14 14:48:17 -0700

    Udemy - Angular 8 - Section 2 - Lesson 21 - Working with Component Styles

--- Udemy/my-first-app/src/app/app.component.ts
+++ Udemy/my-first-app/src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  /*styleUrls: ['./app.component.css']*/
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {
   name = 'Ho Wai Yung';
 }
+
+
+/*
+
+Working with style files or styles, the rule of thumb is that
+
+If it is short, you can stick the style into app.component.ts,
+The perferred way, and to be less messy, it to create a css file
+and reference the css file in styleUrls.
+
+*/
