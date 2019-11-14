6c85d275bad29e7fd9e46b58cd29dfdaa9ef97ef
Ho Wai Yung <howaiyung1988@gmail.com>
2019-11-14 11:07:26 -0700

    Udemy - Angular 8 - Section 2 - Lesson 17 - Creating a New Compenent Pt 2

--- Udemy/my-first-app/src/app/server/server.component.ts
+++ Udemy/my-first-app/src/app/server/server.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
 }) //Class Decorator
 export class ServerComponent {
+
+}*/
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server', // HTML tag, you can use this component in other places to reference from 
+    templateUrl: 'server.component.html' // Template 
+}) //Class Decorator, Pass a Javascript object, store meta-data
+export class ServerComponent{
 
 }
