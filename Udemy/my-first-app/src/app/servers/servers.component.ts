f53a0a400a9e9f12164c7abb384f25709dde48dd
Ho Wai Yung <howaiyung1988@gmail.com>
2019-11-14 14:15:09 -0700

    Udemy - Angular 8 - Section 2 - Lesson 20 - Working with Component Templates

--- Udemy/my-first-app/src/app/servers/servers.component.ts
+++ Udemy/my-first-app/src/app/servers/servers.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  /*templateUrl: './servers.component.html',*/
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>`,
+
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
@@ -13,3 +17,11 @@ export class ServersComponent implements OnInit {
   }
 
 }
+
+
+/*
+In components, '' or single quotations are for single line templates
+             , `` or back quotations are for multi line template components inline
+             Rule of thumb is if you have more than 3 templates using in this component
+
+*/
